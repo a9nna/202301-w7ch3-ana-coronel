bda02c713247241835a8e62d267bd550bc763782
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-22 22:17:31 +0100

    Create errorMiddleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import {
+  generalError,
+  notFoundError,
+} from "./middlewares/errorMiddleware/errorMiddleware";
 
 const app = express();
 
 app.disable("x-powered-by");
+
 app.use(morgan("dev"));
+
+app.use("/users");
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
